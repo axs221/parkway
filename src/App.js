743706d08f036c57d95f5a183c502c3c1222c676
Shawn Axsom <axs221@gmail.com>
2017-08-19 08:22:41 -0400

    Able to handle executing just selected text

--- src/App.js
+++ src/App.js
@@ -4,19 +4,45 @@ import "./App.css";
 import "typeface-roboto";
 
 import Button from "material-ui/Button";
-import TextField from "material-ui/TextField";
+import Grid from "material-ui/Grid";
 import Typography from "material-ui/Typography";
+
+import Editor from "./components/Editor";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { results: "" };
+    this.state = { text: "", results: "" };
   }
 
   handleClick = () => {
-    fetch("http://jsonplaceholder.typicode.com/posts", {
-      method: "GET",
+    let url;
+    let method;
+
+    var ranges = [];
+
+    console.log("this.state.selectedText", this.state.selectedText);
+
+    if (this.state.selectedText) {
+      const tokens = this.state.selectedText.trim().split(" ");
+      console.log("tokens1", tokens);
+      method = tokens[0];
+      url = tokens[1];
+    } else {
+      const tokens = this.state.text.split(" ");
+      console.log("tokens2", this.state);
+      method = tokens[0];
+      url = tokens[1];
+      console.log("tokens2", url, method);
+    }
+
+    if (url.indexOf("http") === -1) {
+      url = `http://${url}`;
+    }
+
+    fetch(url, {
+      method: method,
       mode: "cors"
     }).then(response => {
       response.json().then(json => {
@@ -29,11 +55,24 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <TextField multiline rowsMax="12" />
-        <Button onClick={this.handleClick}>Send Request</Button>
-        <Typography>
-          {this.state.results}
-        </Typography>
+        <Grid container>
+          <Grid item xs={8}>
+            <Editor
+              onChange={event => this.setState({ text: event.target.value })}
+              onSelectionChanged={selectedText => {
+                console.log("selectedText", selectedText());
+
+                this.setState({ selectedText: selectedText() });
+              }}
+            />
+            <Button onClick={this.handleClick}>Send Request</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>
+              {this.state.results}
+            </Typography>
+          </Grid>
+        </Grid>
       </div>
     );
   }
---
+++ src/components/Editor.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+
+import Textarea from "material-ui/Input/Textarea";
+
+export default class Editor extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { text: "" };
+  }
+
+  hasFocus = () => {
+    window.getSelection().toString();
+    const selection = document.getSelection();
+    var ranges = [];
+
+    for (var i = 0; i < selection.rangeCount; i++) {
+      ranges[i] = selection.getRangeAt(i);
+    }
+
+    if (ranges && ranges.length > 0) {
+      const range = ranges[0];
+      return range.startContainer === this.textarea.parentElement;
+    }
+
+    return false;
+  };
+
+  getSelectedText = target => {
+    var text = "";
+    if (window.getSelection) {
+      text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+      text = document.selection.createRange().text;
+    }
+    return text;
+  };
+
+  onChange = (event, value) => {
+    console.log("TARGET", event.target.selectionStart);
+
+    this.setState({
+      text: event.target.value
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange(event, value);
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("selectionchange", event => {
+      if (this.hasFocus()) {
+        this.setState({
+          selectedText: window.getSelection().toString()
+        });
+
+        this.props.onSelectionChanged(this.getSelectedText);
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Textarea
+        className="editor"
+        textareaRef={ref => (this.textarea = ref)}
+        rows={12}
+        style={{ border: "thin solid black", height: "100%" }}
+        value={this.state.text}
+        onChange={this.onChange}
+      />
+    );
+  }
+}
